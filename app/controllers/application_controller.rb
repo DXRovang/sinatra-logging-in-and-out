1d30082aa40c59fc2d63498b9393fdb4b5d03a3d
Alexander Rovang <wolf_vii@yahoo.com>
2021-01-27 22:22:05 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,15 +11,27 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    if User.find_by(username: params[:username])
+      @user = User.find_by(username: params[:username])
+      session[:user_id] = @user.id
+      binding.pry
+      redirect to '/account'
+    else
+      erb :error
+    end
+    # find the user in the database based on their username.
+    # f there is a match, set the session to the user's ID, 
+    # redirect them to the /account route (using redirect to), 
+    # and use ERB to display the user's data on the pag
+    # If there is no match, render the error page.
   end
 
   get '/account' do
-
+    erb :account
   end
 
   get '/logout' do
-
+    session.clear
   end
 
 
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,14 @@
-class Helpers
+require "pry"
+class Helpers 
+
+  def current_user(hash)
+    binding.pry
+    @user = User.find(session[:user_id])
+    # erb :'/users/home'
+  end
+
+  def is_logged_in?
+
+  end
+
 end
